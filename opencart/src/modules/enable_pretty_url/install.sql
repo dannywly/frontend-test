dfafa2514c18cf0cfb524d6795a8f9ad568e404f
danny <dannywang@augmentum.com.cn>
2016-03-04 19:40:39 +0800

    525256

---
+++ opencart/src/modules/enable_pretty_url/install.sql
@@ -0,0 +1,107 @@
+DELETE FROM oc_url_alias WHERE query = 'common/home';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('common/home', ''); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/wishlist';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/wishlist', 'wishlist'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/account';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/account', 'my-account'); 
+
+DELETE FROM oc_url_alias WHERE query = 'checkout/cart';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('checkout/cart', 'shopping-cart'); 
+
+DELETE FROM oc_url_alias WHERE query = 'checkout/checkout';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('checkout/checkout', 'checkout'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/login';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/login', 'login');
+
+DELETE FROM oc_url_alias WHERE query = 'account/logout'; 
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/logout', 'logout'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/order';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/order', 'order-history'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/newsletter';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/newsletter', 'newsletter'); 
+
+DELETE FROM oc_url_alias WHERE query = 'product/special';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('product/special', 'specials'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/account';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/account', 'affiliates');
+
+DELETE FROM oc_url_alias WHERE query = 'checkout/voucher'; 
+INSERT INTO oc_url_alias (query, keyword) VALUES ('checkout/voucher', 'gift-vouchers'); 
+
+DELETE FROM oc_url_alias WHERE query = 'product/manufacturer';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('product/manufacturer', 'brands'); 
+
+DELETE FROM oc_url_alias WHERE query = 'information/contact';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('information/contact', 'contact-us'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/return/insert';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/return/insert', 'request-return');
+
+DELETE FROM oc_url_alias WHERE query = 'information/sitemap'; 
+INSERT INTO oc_url_alias (query, keyword) VALUES ('information/sitemap', 'sitemap'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/forgotten';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/forgotten', 'forgot-password'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/download';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/download', 'downloads'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/return';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/return', 'returns'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/transaction';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/transaction', 'transactions'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/register';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/register', 'create-account'); 
+
+DELETE FROM oc_url_alias WHERE query = 'product/compare';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('product/compare', 'compare-products'); 
+
+DELETE FROM oc_url_alias WHERE query = 'product/search';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('product/search', 'search'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/edit';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/edit', 'edit-account'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/password';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/password', 'change-password'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/address';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/address', 'address-book'); 
+
+DELETE FROM oc_url_alias WHERE query = 'account/reward';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('account/reward', 'reward-points'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/edit';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/edit', 'edit-affiliate-account'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/password';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/password', 'change-affiliate-password'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/payment';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/payment', 'affiliate-payment-options'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/tracking';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/tracking', 'affiliate-tracking-code'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/transaction';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/transaction', 'affiliate-transactions'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/logout';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/logout', 'affiliate-logout'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/forgotten';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/forgotten', 'affiliate-forgot-password'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/register';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/register', 'create-affiliate-account'); 
+
+DELETE FROM oc_url_alias WHERE query = 'affiliate/login';
+INSERT INTO oc_url_alias (query, keyword) VALUES ('affiliate/login', 'affiliate-login');
